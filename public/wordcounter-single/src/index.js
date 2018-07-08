e2247028e0503ac4dcec8d37eb8a0a28a81b6639
Henrik Treadup <henrik.treadup@gmail.com>
2018-07-08 13:17:42 +0200

    Wordcounter example with updating progress bar.

--- public/wordcounter-single/src/index.js
+++ public/wordcounter-single/src/index.js
@@ -18,7 +18,11 @@ function ProgressBar({ completion }) {
     );
 }
 
-function Editor({ text, }) {
+function Editor({ text, onTextChange }) {
+    function handleChange(event) {
+        onTextChange(event.target.value);
+    }
+
     return (
         <div className="flex flex-column mv2">
             <label htmlFor="editor" className="mv2">
@@ -26,6 +30,7 @@ function Editor({ text, }) {
             </label>
             <textarea
                 value={text}
+                onChange={handleChange}
                 id="editor"
             />
         </div>
@@ -36,7 +41,37 @@ function countWords(text) {
     return text ? text.match(/\w+/g).length : 0;
 }
 
-function WordCounter({text, targetWordCount}) {
+class WordCounter extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { text: '' };
+        this.handleTextChange = this.handleTextChange.bind(this);
+    }
+
+    handleTextChange(currentText) {
+        this.setState(() => ({text: currentText}));
+    }
+
+    render() {
+        const { targetWordCount } = this.props;
+        const { text } = this.state;
+        const wordCount = countWords(text);
+        const progress = wordCount / targetWordCount;
+        return (
+            <form className="measure pa4 sans-serif">
+                <Editor onTextChange={this.handleTextChange} text={text} />
+                <div className="flex mt3">
+                    <Counter count={wordCount}/>
+                    <ProgressBar completion={progress}/>
+                </div>
+            </form>
+        );
+    }
+
+}
+
+function OldWordCounter({text, targetWordCount}) {
     const wordCount = countWords(text);
     const progress = wordCount / targetWordCount;
     return (
@@ -51,6 +86,6 @@ function WordCounter({text, targetWordCount}) {
 }
 
 ReactDOM.render(
-    <WordCounter text="Count the words in here." targetWordCount={10} />,
+    <WordCounter targetWordCount={10} />,
     document.getElementById('app')
 );
